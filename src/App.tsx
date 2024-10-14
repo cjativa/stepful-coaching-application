ba7b47e804e2833817b252fce707bff19a3bfeb6
Christopher Jativa <christopher.jativa320@gmail.com>
2024-10-14 16:53:50 -0400

    working on time and date pickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 import { LoginPage } from "./pages";
+import { CoachBooking } from "./pages/booking";
+
+const StyledBackground = styled(Box)(({ theme }) => ({
+  width: "100%",
+  height: "100vh",
+  background: "#7c8ef2",
+
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
 
 export const Application = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StyledBackground>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/booking/coach" element={<CoachBooking />} />
+        </Routes>
+      </StyledBackground>
+    </LocalizationProvider>
   );
 };
