c13f1d473ca1b915bbaa3efc69d6de57dd6be46e
Christopher Jativa <christopher.jativa320@gmail.com>
2024-10-15 19:00:21 -0400

    define our needed routes

--- server/api/routes/scheduleRouter.ts
+++ server/api/routes/scheduleRouter.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 
-// import { IntentController } from "../controllers";
+import { ScheduleController } from "../controllers";
 
 const ScheduleRouter = Router();
+
+ScheduleRouter.get("/", ScheduleController.handleFetchForCoach);
+ScheduleRouter.get("/student", ScheduleController.handleFetchForStudent);
+ScheduleRouter.post("/", ScheduleController.handleCreate);
+ScheduleRouter.post("/book", ScheduleController.handleBook);
+ScheduleRouter.post("/complete", ScheduleController.handleCompletion);
 
 export { ScheduleRouter };
