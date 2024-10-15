a7502e54e99c8db9e8b652445a9644a5f2b5676a
Christopher Jativa <christopher.jativa320@gmail.com>
2024-10-15 00:14:06 -0400

    implement login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -7,6 +7,11 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+import { useAuthentication } from "../../hooks";
 
 const StyledLoginPaper = styled(Paper)(({ theme }) => ({
   width: "400px",
@@ -17,20 +22,16 @@ const StyledLoginPaper = styled(Paper)(({ theme }) => ({
   textAlign: "center",
 }));
 
-const StyledBackground = styled(Box)(({ theme }) => ({
-  width: "100%",
-  height: "100vh",
-  background: "#7c8ef2",
-
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-}));
-
 const StyledTabPanel = styled("div")(({}) => ({
   padding: "3em",
   height: "600px",
+}));
+
+const StyledInputContainer = styled("div")(({}) => ({
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: "1",
+  rowGap: "2em",
 }));
 
 interface TabPanelProps {
@@ -50,48 +51,103 @@ function TabPanel(props: TabPanelProps) {
       id={`full-width-tabpanel-${index}`}
       aria-labelledby={`full-width-tab-${index}`}
     >
-      {value === index && (
-        <Box>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <>{children}</>}
     </StyledTabPanel>
   );
 }
 
 export const Login = () => {
+  const { login } = useAuthentication();
   const theme = useTheme();
-  const [value, setValue] = React.useState(1);
+  const navigate = useNavigate();
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  const [loginName, setLoginName] = React.useState("");
+  const [loginError, setLoginError] = React.useState(false);
+  const [selectedTab, setSelectedTab] = React.useState<number>(1);
+
+  const handleTabChange = (
+    event: React.SyntheticEvent,
+    newTabValue: number
+  ) => {
+    // Handle changing of the tab selection
+    // We'll also clear out the entered login name when
+    // the end-user switches between login tabs
+    setLoginName("");
+    setSelectedTab(newTabValue);
+  };
+
+  const onLoginButtonClick = () => {
+    const loginIsSuccessful = login(loginName);
+
+    // We'll navigate to the next step if login was successful
+    if (loginIsSuccessful) {
+      setLoginError(false);
+      navigate("/booking/coach");
+    }
+
+    // Otherwise, unsuccessful login, let's indicate a basic error
+    else {
+      setLoginError(true);
+    }
+  };
+
+  const onLoginInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginName(event.target.value);
+  };
+
+  const LoginErrorMessage = () => {
+    return (
+      <Typography>Could not login with the provided credential</Typography>
+    );
   };
 
   return (
-    <StyledBackground>
-      <Stack>
-        <StyledLoginPaper variant="outlined">
-          <TabPanel value={value} index={0} dir={theme.direction}>
-            Coach Login
-          </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-            Student Login
-          </TabPanel>
-          <AppBar position="static">
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              indicatorColor="secondary"
-              textColor="inherit"
-              variant="fullWidth"
-              aria-label="full width tabs example"
-            >
-              <Tab label="Coach Login" />
-              <Tab label="Student Sign-in" />
-            </Tabs>
-          </AppBar>
-        </StyledLoginPaper>
-      </Stack>
-    </StyledBackground>
+    <Stack>
+      <StyledLoginPaper variant="outlined">
+        <TabPanel value={selectedTab} index={0} dir={theme.direction}>
+          <StyledInputContainer>
+            <p>Coach Login</p>
+            <TextField
+              id="coach-name"
+              label="Coach Name"
+              variant="filled"
+              onChange={onLoginInputChange}
+            />
+            <Button variant="contained" onClick={onLoginButtonClick}>
+              Login
+            </Button>
+            {loginError ? <LoginErrorMessage /> : null}
+          </StyledInputContainer>
+        </TabPanel>
+        <TabPanel value={selectedTab} index={1} dir={theme.direction}>
+          <StyledInputContainer>
+            <p>Student Login</p>
+            <TextField
+              id="student-name"
+              label="Student Name"
+              variant="filled"
+              onChange={onLoginInputChange}
+            />
+            <Button variant="contained" onClick={onLoginButtonClick}>
+              Login
+            </Button>
+            {loginError ? <LoginErrorMessage /> : null}
+          </StyledInputContainer>
+        </TabPanel>
+        <AppBar position="static">
+          <Tabs
+            value={selectedTab}
+            onChange={handleTabChange}
+            indicatorColor="secondary"
+            textColor="inherit"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="Coach Login" />
+            <Tab label="Student Sign-in" />
+          </Tabs>
+        </AppBar>
+      </StyledLoginPaper>
+    </Stack>
   );
 };
