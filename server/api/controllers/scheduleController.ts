995d86a8db780da27e5f9d87278eb0455450644c
Christopher Jativa <christopher.jativa320@gmail.com>
2024-10-15 18:59:55 -0400

    working on business logic for coach schedule creation and retrieval

---
+++ server/api/controllers/scheduleController.ts
@@ -0,0 +1,77 @@
+import express from "express";
+import dayjs from "dayjs";
+
+import { ScheduleService } from "../services";
+
+export class ScheduleController {
+  /** Separate class to handle the request and responses from our Express server
+   * We have this as a distinct class to enable us to perform initial validation of
+   * incoming request payloads before moving onto the business logic layer
+   */
+
+  public static async handleCreate(
+    request: express.Request,
+    response: express.Response
+  ) {
+    try {
+      const coachId = request.body.coachId;
+      const startTime = new Date(request.body.startTime);
+      const endTime = new Date(request.body.endTime);
+
+      const scheduleItem = await ScheduleService.handleCreate(
+        coachId,
+        startTime,
+        endTime
+      );
+
+      return response.status(200).json({
+        startTime: scheduleItem.startTime,
+        endTime: scheduleItem.endTime,
+        id: scheduleItem.id,
+      });
+    } catch (error) {
+      return response
+        .status(400)
+        .json("Could not create schedule item for coach");
+    }
+  }
+
+  public static async handleFetchForCoach(
+    request: express.Request,
+    response: express.Response
+  ) {
+    try {
+      const coachId = request.body.coachId;
+      const scheduleItemList = await ScheduleService.handleFetchForCoach(
+        coachId
+      );
+
+      return response.status(200).json(scheduleItemList);
+    } catch (error) {
+      return response
+        .status(400)
+        .json("Could not retrieve schedule list for specified coach");
+    }
+  }
+
+  public static async handleFetchForStudent(
+    request: express.Request,
+    response: express.Response
+  ) {
+    return response.status(200).json({});
+  }
+
+  public static async handleBook(
+    request: express.Request,
+    response: express.Response
+  ) {
+    return response.status(200).json({});
+  }
+
+  public static async handleCompletion(
+    request: express.Request,
+    response: express.Response
+  ) {
+    return response.status(200).json({});
+  }
+}
---
+++ server/api/services/scheduleService.ts
@@ -0,0 +1,31 @@
+import { prismaClient } from "./prismaClient";
+
+export class ScheduleService {
+  /** Performs creation of a new schedule item for a Coach */
+  public static async handleCreate(
+    coachId: string,
+    startTime: Date,
+    endTime: Date
+  ) {
+    const scheduleItem = await prismaClient.coachSchedule.create({
+      data: {
+        coachId,
+        startTime,
+        endTime,
+      },
+    });
+
+    return scheduleItem;
+  }
+
+  /** Performs retrieval of the existing schedule items for a Coach */
+  public static async handleFetchForCoach(coachId: string) {
+    const scheduleItem = await prismaClient.coachSchedule.findMany({
+      where: {
+        coachId,
+      },
+    });
+
+    return scheduleItem;
+  }
+}
